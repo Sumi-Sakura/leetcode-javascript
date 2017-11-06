0791a8903b7840a75d18a5451fdf99007c4db205
sumi <1228668328@qq.com>
2017-11-06 11:52:47 +0800

    更新解题思路

--- 681.Next Closest Time/Next Closest Time.md
+++ 681.Next Closest Time/Next Closest Time.md
@@ -15,4 +15,16 @@ You may assume the given input string is always valid. For example, "01:34", "12
     Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22. It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
 
 ## 简译
-给你一个时间字符串，需要返回距离该近且在该时间之后的时间
+给你一个时间字符串，需要返回距离该近且在该时间之后的时间，且返回的时间内的数字要为原字符串所拥有的数字
+
+## 思路
+1.	假设传入的时间为 `ab:cd` ，那么需要找到最近的时间，最开始肯定要从 `d` 开始改变，如果 `a`、`b`、`c` 中存在比 `d` 大的值，那么直接将这个值赋值给 `d` 即可
+
+2.	如果不存在比 `d` 大的值，那么就要去找比 `c` 大的值并去替换 `c`，这时需要注意两点
+	-	如果存在某个值比 `c` 大，替换后，时间是需要合法的，因为 `c` 不能大于 5
+	-	如果存在某个值比 `c` 大，替换后，`d` 也要相应改变，因为 `c` 已经比原有的值要大了，所以最接近的值应该是 `d` 改为`a`、`b`、`c`、`d` 中的最小值
+
+3.	同理，`c` 如果也无法替换，那么要去替换小时的数字，这里需要注意一点
+	-	小时的十分位是不能大于 3 的，且当小时时分位为 2 时，小时的个位不能大于 3
+
+4.	当四个值都找不到合适的值（既接近又合法）去替换时，那么只能返回由 4 个最小值拼接而成的时间
--- 681.Next Closest Time/NextClosestTime.js
+++ 681.Next Closest Time/NextClosestTime.js
@@ -6,8 +6,13 @@ const toNumber = (str) => {
 // 处理时间字符串并返回全为数字的数组
 const timeStrToTimeArr = (str) => {
 	let timeNumArr = []
+	// 分割数组并循环
 	str.split('').forEach((item) => {
-		if (item !== ':') timeNumArr.push(toNumber(item))
+		// 去掉冒号
+		if (item !== ':') {
+			// 将数组内的数字字符都转换为数字类型
+			timeNumArr.push(toNumber(item))
+		}
 	})
 	return timeNumArr
 }
@@ -22,19 +27,24 @@ const sortOutTimeArr = (arr) => {
 	return allNumArr
 }
 
+// 校验时间的准确性
 const timeValidate = (arr) => {
+	// 小时的第一个数字不能大于 2
 	if (arr[0] > 2) {
 		return false
 	}
-	if (arr[0] === 2 && arr[1] > 4) {
+	// 小时的第一个数字为 2 时，第二个数字不能大于 3
+	if (arr[0] === 2 && arr[1] > 3) {
 		return false
 	}
+	// 分钟的第三个数字不能大于 5
 	if (arr[2] > 5) {
 		return false
 	}
 	return true
 }
 
+// 将数字数组添加冒号后转换为字符串输出
 const timeArrToTimeStr = (arr) => {
 	let timeArr = [...arr]
 	timeArr.splice(2, 0, ':')
@@ -46,29 +56,39 @@ const nextClosestTime = (time) => {
 	let closestTimeArr = timeStrToTimeArr(time)
 	let allNumArr = sortOutTimeArr(closestTimeArr)
 	let minNum = 0
+	// 总共 4 个数字，且需要从最后一位开始寻找最接近且在该时间之后的时间
 	let index = 3
 	// 最小值为排序后的数组中 index 为 0 的值
 	minNum = allNumArr[0]
 	while(index >= -1) {
-		let closestIndex = allNumArr.indexOf(closestTimeArr[index])
-		let closestNum = allNumArr[closestIndex + 1]
+		// 找到当前数字在存有所有数字中的数组中的索引值，+1 以后就是最接近的数字的索引值
+		let closestIndex = allNumArr.indexOf(closestTimeArr[index]) + 1
+		// 
+		let closestNum = allNumArr[closestIndex]
+		// 如果索引值大于 -1 则说明并没有循环完整个数字
 		if (index > -1) {
+			// 如果不为最后一位
 			if (closestTimeArr[index + 1]) {
+				// 则将它的后一位数字置为最小值
 				closestTimeArr[index + 1] = minNum
 			}
+			// 如果最接近的数字存在
 			if (closestNum) {
+				// 则将当前位置赋值为最接近的数字
 				closestTimeArr[index] = closestNum
+				// 并对时间的合法性进行校验
 				if (timeValidate(closestTimeArr)) {
-					// return timeArrToTimeStr(closestTimeArr)
+					// 如果校验通过则返回最接近的时间
 					return timeArrToTimeStr(closestTimeArr)
 				}
 			}
+		// 循环完整个数组都没有找到合适的时间则返回全为最小值的时间
 		} else {
+			// 后面三位已经在循环中被置为最小值，所以这里只需要将首位置为最小值即可
 			closestTimeArr[0] = minNum
-			// return timeArrToTimeStr(closestTimeArr)
 			return timeArrToTimeStr(closestTimeArr)
 		}
 		index--
 	}
 }
-console.log(nextClosestTime('19:29'))
+console.log(nextClosestTime('23:49'))
